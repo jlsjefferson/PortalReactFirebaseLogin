e4def6a0a57e0ca902649bcb4579e5bd699fe41f
jefferson lucio <jeferson.silva@tora.com.br>
2020-06-23 09:28:12 -0300

    base update

--- src/firebase/base.js
+++ src/firebase/base.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-apiKey:  "AIzaSyC8ZjR3MONT-sxv85ausWTyUiFyDmPy9ks",
-authDomain: "autenticacao-3ef9f.firebaseapp.com",
-databaseURL: "https://autenticacao-3ef9f.firebaseio.com",
-projectId: "autenticacao-3ef9f",
+apiKey:  "AIzbC9ZjR6MONT-sxv85ausWTyUiFyDPy9ks",
+authDomain: "autenticacao-3ef9f.firebasaapp.com",
+databaseURL: "https://autenticacao-ef9f.firebaseio.com",
+projectId: "autenticacao-aef1d",
 storageBucket: "",
 messagingSenderId: "814231108098",
-appId: "1:814231108098:web:f371ffe6a425ae05"
+appId: "1:814230008198:web:f371ffe6a425ae05"
 
 });
 
